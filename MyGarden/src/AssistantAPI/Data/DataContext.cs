fd0bd980a9706de69e729f11e35845865c6691a5
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2025-02-25 11:21:48 +0300

    assistantApi base added

---
+++ MyGarden/src/AssistantAPI/Data/DataContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AssistantAPI.Data
+{
+    public class DataContext(BaseConfiguration configuration) : DbContext
+    {
+        private BaseConfiguration Configuration { get; } = configuration;
+
+        /// <summary>
+        ///     Обработать настройку сессии.
+        /// </summary>
+        /// <param name="optionsBuilder">Набор интерфейсов настройки сессии.</param>
+        /// <exception cref="Exception">При ошибке подключения.</exception>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Configuration.ConfigureContext(optionsBuilder);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        /// <summary>
+        ///     Попытаться асинхронно инициализировать сессию.
+        ///     Используется для проверки подключения
+        ///     и инициализации структуры таблиц.
+        /// </summary>
+        /// <returns>Статус успешности инициализации.</returns>
+        public async Task<bool> TryInitializeAsync()
+        {
+            try
+            {
+                await Database.MigrateAsync();
+                return await Database.CanConnectAsync();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///     Обработать инициализацию модели.
+        ///     Используется для дополнительной настройки модели.
+        /// </summary>
+        /// <param name="modelBuilder">Набор интерфейсов настройки модели.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new Group.Configuration(Configuration));
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //public DbSet<Group> Groups => Set<Group>();
+    }
+}
+
+
+
+
---
+++ MyGarden/src/AssistantAPI/Program.cs
@@ -0,0 +1,61 @@
+﻿using AssistantAPI.Data;
+using AssistantAPI.Middleware;
+using Prometheus;
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddEnvironmentVariables();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+RegisterCoreServices(builder.Services);
+RegisterDataSources(builder.Services);
+
+var application = builder.Build();
+
+application.UseMiddleware<ExceptionHandler>();
+application.UseSwagger();
+application.UseSwaggerUI();
+application.MapControllers();
+application.MapHealthChecks("/health");
+application.UseCors();
+
+await InitializeDataSources(application);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo
+    .GrafanaLoki("http://loki:3100")
+    .CreateLogger();
+
+application.UseMetricServer(url: "/metrics");
+application.UseHttpMetrics();
+
+application.Run();
+
+void RegisterCoreServices(IServiceCollection services)
+{
+    //services.AddScoped<GrowStageServive>();
+    services.AddControllers();
+}
+
+void RegisterDataSources(IServiceCollection services)
+{
+    var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+    var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+    var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+    var dbPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+    var connectionString = $"Server={dbHost};Port=5432;Database={dbName};User Id={dbUser};Password={dbPassword};";
+    services.AddScoped(provider => new DataContext(new ContextConfiguration(connectionString)));
+}
+
+async Task InitializeDataSources(WebApplication application)
+{
+    using var scope = application.Services.CreateScope();
+    var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await dataContext.TryInitializeAsync();
+}
