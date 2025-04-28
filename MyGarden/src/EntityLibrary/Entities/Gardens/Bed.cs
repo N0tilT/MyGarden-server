7b62fa90d5ff7c411c3900a714c30ae30e9ede8f
Gromova Polina <90103780+N0tilT@users.noreply.github.com>
2025-04-28 10:34:54 +0300

    flowerbed model types changed to double

--- MyGarden/src/EntityLibrary/Entities/Gardens/Bed.cs
+++ MyGarden/src/EntityLibrary/Entities/Gardens/Bed.cs
@@ -3,11 +3,11 @@
     public class Bed
     {
         public int? Id { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
-        public int RotationAngle { get; set; }
+        public double RotationAngle { get; set; }
         public string AdditionalInfo { get; set; } = string.Empty;
         public List<int> Plants { get; set; } = [];
     }
